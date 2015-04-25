afc7b1598c2fe6b8df9efebb0559d116b6128904
Mickey Reiss <mickey.reiss@getbraintree.com>
2015-04-24 18:36:22 -0700

    Create outline for BTPayPal integration tests with PayPalOneTouchCore

--- Specs/Braintree-PayPal-Integration-Specs/BTPayPalIntegrationSpec.m
+++ Specs/Braintree-PayPal-Integration-Specs/BTPayPalIntegrationSpec.m
@@ -1,132 +1,103 @@
-#import "BTClient+BTPayPal.h"
-#import "BTClient+Testing.h"
-#import "BTClient+Offline.h"
-#import "BTClientToken.h"
-#import "PayPalMobile.h"
-#import "BTPayPalViewController_Internal.h"
-#import "BTErrors+BTPayPal.h"
-
 SpecBegin(BTPayPal_Integration)
 
+it(@"is untested", ^{
+    XCTFail(@"write tests for BTPayPal");
+});
 
-describe(@"preparePayPalMobile", ^{
-    describe(@"online", ^{
-        describe(@"with PayPal enabled", ^{
-            __block BTClient *testClient;
+describe(@"PayPal One Touch Core", ^{
+    describe(@"future payments", ^{
+        describe(@"performing app switches", ^{
+            it(@"performs an app switch to PayPal when the PayPal app is installed", ^{
+            });
 
-            beforeEach(^{
-                waitUntil(^(DoneCallback done){
-                    [BTClient testClientWithConfiguration:@{ BTClientTestConfigurationKeyMerchantIdentifier:@"altpay_merchant",
-                                                             BTClientTestConfigurationKeyPublicKey:@"altpay_merchant_public_key",
-                                                             BTClientTestConfigurationKeyCustomer:@YES }
-                                               async:@YES
-                                               completion:^(BTClient *client) {
-                                                   testClient = client;
-                                                   done();
-                                               }];
+            it(@"performs an app switch to Safari when the PayPal app is not installed", ^{
+            });
+
+            it(@"notifies the delegate when an app switch will take place", ^{
+            });
+
+            it(@"fails to switch if the returnUrlScheme is not valid, returning the error to the delegate", ^{
+            });
+
+            describe(@"analytics", ^{
+                it(@"posts an analytics event for a successful app switch to the PayPal app", ^{
+                });
+
+                it(@"posts an analytics event for a successful app switch to the Browser", ^{
+                });
+
+                it(@"posts an analytics event for a failed app switch", ^{
+                });
+
+                it(@"posts analytics events when preflight checks fail", ^{
                 });
             });
+        });
 
-            it(@"configures BTClient for use with PayPal based on the client token configuration", ^{
-                OCMockObject *mockPayPalMobile = [OCMockObject mockForClass:[PayPalMobile class]];
-
-                // Assert that Braintree environment is added
-                [[[mockPayPalMobile expect] classMethod] addEnvironments:[OCMArg checkWithBlock:^BOOL(NSDictionary *environments) {
-                    return [environments count] == 1 && [NSURL URLWithString:environments[@"Braintree"][@"api"]] != nil;
-                }]];
-
-                // Assert that Braintree environment is configured with a client id
-                [[[mockPayPalMobile expect] classMethod] initializeWithClientIdsForEnvironments:[OCMArg checkWithBlock:^BOOL(NSDictionary *clientIdsForEnvironments) {
-                    return [clientIdsForEnvironments count] == 1 && [clientIdsForEnvironments[@"Braintree"] length] > 0;
-                }]];
-
-                // Assert that Braintree environment is actually used
-                [[[mockPayPalMobile expect] classMethod] preconnectWithEnvironment:@"Braintree"];
-
-                [testClient btPayPal_preparePayPalMobileWithError:NULL];
-
-                [mockPayPalMobile verify];
-                [mockPayPalMobile stopMocking];
+        describe(@"classifying app switch returns", ^{
+            it(@"accepts return URLs from the browser", ^{
             });
 
-            it(@"enables BTPayPalViewController to embed a PayPalProfileSharingViewController that hits the specified environment", ^{
-                OCMockObject *mockPayPalMobile = [OCMockObject mockForClass:[PayPalMobile class]];
-                [[[mockPayPalMobile stub] classMethod] preconnectWithEnvironment:[OCMArg isNotNil]];
+            it(@"accepts return URLs from the app", ^{
+            });
 
-                BTPayPalViewController *payPalViewController = [[BTPayPalViewController alloc] initWithClient:testClient];
+            it(@"rejects other return URLs", ^{
+            });
 
-                [payPalViewController view];
+            it(@"rejects other malformed URLs", ^{
+            });
 
-                expect(payPalViewController.payPalProfileSharingViewController).to.beKindOf([PayPalProfileSharingViewController class]);
-                [mockPayPalMobile stopMocking];
+            it(@"rejects URLs when no app switch has taken place", ^{
+            });
+
+            it(@"ignores the case of the URL Scheme to account for Safari's habit of downcasing URL schemes", ^{
             });
         });
 
-        describe(@"with PayPal disabled", ^{
-            __block BTClient *testClient;
+        describe(@"handling app switch returns", ^{
+            it(@"ignores an irrelevant or malformed URL", ^{
+            });
 
-            beforeEach(^{
-                waitUntil(^(DoneCallback done){
-                    NSString *merchantIdWithPayPalDisabled = @"integration2_merchant_id";
-                    NSString *merchantKeyWithPayPalDisabled = @"integration2_public_key";
-                    [BTClient testClientWithConfiguration:@{ BTClientTestConfigurationKeyMerchantIdentifier: merchantIdWithPayPalDisabled,
-                                                             BTClientTestConfigurationKeyPublicKey:merchantKeyWithPayPalDisabled,
-                                                             BTClientTestConfigurationKeyCustomer: @YES }
-                                               async:@YES
-                                               completion:^(BTClient *client) {
-                                                   testClient = client;
-                                                   done();
-                                               }];
+            it(@"accepts a success app switch return", ^{
+            });
+
+            it(@"accepts a failure app switch return", ^{
+            });
+
+            it(@"accepts a cancelation app switch return", ^{
+            });
+
+            it(@"tokenizes a success response, returning the payment method nonce to the developer", ^{
+            });
+
+            it(@"returns tokenization failures to the developer", ^{
+            });
+
+            it(@"returns a failure to the developer", ^{
+            });
+
+            it(@"returns a cancelation to the developer", ^{
+            });
+
+            describe(@"analytics", ^{
+                it(@"post an analytics event to indicate a successful returns", ^{
+                });
+
+                it(@"post an analytics event to indicate a failure returns", ^{
+                });
+
+                it(@"post an analytics event to indicate a cancelation return", ^{
+                });
+
+                it(@"posts an anlaytics event to indicate tokenization success", ^{
+                });
+
+                it(@"posts an anlaytics event to indicate tokenization failure", ^{
                 });
             });
-
-            it(@"fails to initialize if the paypal directBaseURL is not present", ^{
-                OCMockObject *mockPayPalMobile = [OCMockObject mockForClass:[PayPalMobile class]];
-                [[[mockPayPalMobile stub] classMethod] preconnectWithEnvironment:[OCMArg isNotNil]];
-                id mockPayPalViewControllerDelegate = [OCMockObject mockForProtocol:@protocol(BTPayPalViewControllerDelegate)];
-
-                BTPayPalViewController *payPalViewController = [[BTPayPalViewController alloc] initWithClient:testClient];
-                payPalViewController.delegate = mockPayPalViewControllerDelegate;
-
-                [[mockPayPalViewControllerDelegate stub] payPalViewController:payPalViewController didFailWithError:[OCMArg checkWithBlock:^BOOL(NSError *error) {
-                    return error.domain == BTBraintreePayPalErrorDomain && error.code == BTMerchantIntegrationErrorPayPalConfiguration;
-                }]];
-                [payPalViewController view];
-
-                expect(payPalViewController.view).to.beNil();
-
-                [mockPayPalMobile stopMocking];
-                [mockPayPalViewControllerDelegate stopMocking];
-            });
-        });
-    });
-
-    describe(@"offline", ^{
-        __block BTClient *client;
-
-        beforeEach(^{
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-            client = [[BTClient alloc] initWithClientToken:[BTClient btPayPal_offlineTestClientToken]];
-#pragma clang diagnostic pop
         });
 
-        it(@"defaults to PayPal mock mode in BTClient offline mode", ^{
-            OCMockObject *mockPayPalMobile = [OCMockObject mockForClass:[PayPalMobile class]];
-
-            // Assert that Braintree environment is actually used
-            [[[mockPayPalMobile expect] classMethod] preconnectWithEnvironment:PayPalEnvironmentNoNetwork];
-
-            [client btPayPal_preparePayPalMobileWithError:NULL];
-
-            [mockPayPalMobile verify];
-            [mockPayPalMobile stopMocking];
-        });
-
-        it(@"enables BTPayPalViewController to embed a PayPalProfileSharingViewController that hits the mock mode environment", ^{
-            BTPayPalViewController *payPalViewController = [[BTPayPalViewController alloc] initWithClient:client];
-            [payPalViewController view];
-            expect(payPalViewController.payPalProfileSharingViewController).to.beKindOf([PayPalProfileSharingViewController class]);
+        describe(@"checking if app switch is available", ^{
         });
     });
 });
